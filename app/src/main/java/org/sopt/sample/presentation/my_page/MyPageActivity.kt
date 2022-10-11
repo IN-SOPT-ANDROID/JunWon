c097ba095058620dfe1d10f695eac43b8e6f22cb
murjune <sis9221302@gmail.com>
2022-10-12 07:20:32 +0900

    [feat] 바텀네비 연동

--- app/src/main/java/org/sopt/sample/presentation/my_page/MyPageActivity.kt
+++ app/src/main/java/org/sopt/sample/presentation/my_page/MyPageActivity.kt
@@ -1,27 +1,57 @@
 package org.sopt.sample.presentation.my_page
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.sample.R
 import org.sopt.sample.databinding.ActivityMyPageBinding
-import org.sopt.sample.presentation.setting.SettingActivity
+import org.sopt.sample.presentation.my_page.gallery.GalleryFragment
+import org.sopt.sample.presentation.my_page.home.HomeFragment
+import org.sopt.sample.presentation.my_page.search.SearchFragment
 import org.sopt.sample.util.binding.BindingActivity
+import timber.log.Timber
 
 @AndroidEntryPoint
 class MyPageActivity : BindingActivity<ActivityMyPageBinding>(R.layout.activity_my_page) {
 
-    private val viewModel by viewModels<MyPageViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding.vm = viewModel
+        initDefaultFragment()
         initClickListener()
     }
 
+    private fun initDefaultFragment() {
+        supportFragmentManager.commit {
+            replace<HomeFragment>(R.id.fc_my_page)
+        }
+    }
+
     private fun initClickListener() {
-        binding.ivSetting.setOnClickListener {
-            startActivity(Intent(this, SettingActivity::class.java))
+        with(binding.botNavMain) {
+            setOnItemSelectedListener {
+                checkItemId(it.itemId)
+            }
+            setOnItemReselectedListener {
+                //TODO ScrollTOUP 구현
+            }
+        }
+    }
+
+    private fun checkItemId(itemId: Int): Boolean {
+        when (itemId) {
+            R.id.navigation_home -> navigateTo<HomeFragment>()
+            R.id.navigation_gallery -> navigateTo<GalleryFragment>()
+            R.id.navigation_search -> navigateTo<SearchFragment>()
+            else -> Timber.e(IllegalArgumentException("itemId: $itemId"))
+        }
+        return true
+    }
+
+    private inline fun <reified T : Fragment> navigateTo() {
+        supportFragmentManager.commit {
+            replace<T>(R.id.fc_my_page, T::class.java.canonicalName)
         }
     }
 }

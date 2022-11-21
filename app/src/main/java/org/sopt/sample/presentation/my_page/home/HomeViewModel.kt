2827a994766ff534f0ba6f41e9926463d961edfd
이준원 <sis9221302@gmail.com>
2022-11-22 08:13:03 +0900

    [feat] Loading, Error, Empty, Success 상태 처리

--- app/src/main/java/org/sopt/sample/presentation/my_page/home/HomeViewModel.kt
+++ app/src/main/java/org/sopt/sample/presentation/my_page/home/HomeViewModel.kt
@@ -6,12 +6,14 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import org.sopt.sample.data.entity.Follower
 import org.sopt.sample.data.entity.FollowerHeader
 import org.sopt.sample.data.entity.User
 import org.sopt.sample.data.repository.FollowerRepository
 import org.sopt.sample.data.repository.UserRepository
+import org.sopt.sample.util.UiState
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -21,21 +23,34 @@ class HomeViewModel @Inject constructor(
     followerRepository: FollowerRepository
 ) :
     ViewModel() {
-    private var _followerList: MutableStateFlow<List<Follower>> =
-        MutableStateFlow(listOf(FollowerHeader()))
-    val followerList: StateFlow<List<Follower>> = _followerList.asStateFlow()
     private var _user = MutableStateFlow(userRepository.getUser())
     val user: StateFlow<User> = _user.asStateFlow()
+    private var _uiState: MutableStateFlow<UiState<HomeUiStateModel>> = MutableStateFlow(UiState.Loading)
+    val uiState = _uiState.asStateFlow()
 
     init {
         viewModelScope.launch {
             followerRepository.getFollowers()
                 .onSuccess { followers ->
-                    _followerList.value = listOf(FollowerHeader()) + followers
+                    if (followers.isEmpty()) {
+                        _uiState.update {
+                            UiState.Empty
+                            return@onSuccess
+                        }
+                    }
+                    _uiState.update { UiState.Empty }
+//                    _uiState.update { UiState.Success(HomeUiStateModel(listOf(FollowerHeader()) + followers)) }
                 }
-                .onFailure {
-                    Timber.e(it)
+                .onFailure { throwable ->
+                    Timber.e(throwable)
+                    _uiState.update {
+                        UiState.Error(throwable)
+                    }
                 }
         }
     }
+
+    data class HomeUiStateModel(
+        val followers: List<Follower> = emptyList()
+    )
 }

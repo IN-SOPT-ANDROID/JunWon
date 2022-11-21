2ac82562485ac6b69b0add9e89fe1bed97ea8a36
이준원 <sis9221302@gmail.com>
2022-11-22 08:10:53 +0900

    [ui/feat] LoadingDialog 구현

---
+++ app/src/main/java/org/sopt/sample/util/dialog/LoadingDialogFragment.kt
@@ -0,0 +1,38 @@
+package org.sopt.sample.util.dialog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import org.sopt.sample.R
+import org.sopt.sample.databinding.DialogLoadingBinding
+
+class LoadingDialogFragment : DialogFragment() {
+    private var _binding: DialogLoadingBinding? = null
+    private val binding get() = _binding ?: error(getString(R.string.binding_error))
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DialogLoadingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        isCancelable = false
+        dialog?.window?.setBackgroundDrawableResource(android.R.color.transparent)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        const val TAG = "LoadingDialogFragment"
+    }
+}
